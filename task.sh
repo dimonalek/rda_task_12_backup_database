77322cbde2b47c791d84d5af46d1a9374e47610d
dimonalek <dimonalek@gmail.com>
2026-04-24 11:37:03 +0300

    Created backup script

--- task.sh
+++ task.sh
@@ -1,1 +1,12 @@
 #! /bin/bash
+
+DB_USER="$DB_USER"
+DB_PASSWORD="$DB_PASSWORD"
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-db ShopDB > full_backup.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < full_backup.sql
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-db --no-create-info ShopDB > data_backup.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < data_backup.sql
